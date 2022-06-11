547f2026b4aebd3ad0b882a7a2d6a6a0b0b58356
dubipy <qnfrha999@naver.com>
2022-06-11 23:32:48 +0900

    .

---
+++ src/ex-03.js
@@ -0,0 +1,9 @@
+import * as THREE from 'three'
+import { WEBGL } from './webgl'
+
+if (WEBGL.isWebGLAvailable()) {
+
+} else {
+  var warning = WEBGL.getWebGLErrorMessage()
+  document.body.appendChild(warning)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const path = require('path') //path를 가져온다
 
-module.exports = {
+module.exports = { //module exports 부분에
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/index.js', //src/index.js를 어떻게 내보낼 것인가?
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'), //public 폴더 안에다가
+    filename: 'bundle.js', // bundle.js를 변환을 시켜준다는 의미
   },
   performance: {
     maxEntrypointSize: 1024000,
